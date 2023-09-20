0721dc45e82e96de35188386d6512f4154995f95
LAPTOP-K2DUVB61\PC <tho.nguyenhuu03@gmail.com>
2023-09-20 22:59:30 +0700

    Init Ex9

--- Src/Core/Src/main.c
+++ Src/Core/Src/main.c
@@ -118,6 +118,48 @@ void setNumberOnClock(int num) {
 
 			  }
 }
+void clearNumberOnClock(int num) {
+	switch(num){
+				case 0:
+					HAL_GPIO_WritePin(LED_0_GPIO_Port, LED_0_Pin, SET);
+					break;
+				case 1:
+					HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+					break;
+				case 2:
+					HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+					break;
+				case 3:
+					HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
+					break;
+				case 4:
+					HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+					break;
+				case 5:
+					HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+					break;
+				case 6:
+					HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+					break;
+				case 7:
+					HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+					break;
+				case 8:
+					HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+					break;
+				case 9:
+					HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+					break;
+				case 10:
+					HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+					break;
+				case 11:
+					HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+					break;
+				default: break;
+
+			  }
+}
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -157,9 +199,9 @@ int main(void)
 	  if(counter <=0 ) {
 		  setNumberOnClock(num);
 		  counter = 100;
+		  if(num%2==0) clearNumberOnClock(num);
 		  num++;
-		  if(num == 5)
-		  clearAllClock();
+
 
 	  }
 	  counter--;
