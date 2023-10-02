7acb80bbe868b1ab3bf4a1f6aec3bce242a561c1
LAPTOP-K2DUVB61\PC <tho.nguyenhuu03@gmail.com>
2023-10-02 16:37:54 +0700

    Update Ex5

--- Src/Core/Src/main.c
+++ Src/Core/Src/main.c
@@ -99,13 +99,6 @@ int main(void)
   MX_GPIO_Init();
   /* USER CODE BEGIN 2 */
 
-	HAL_GPIO_WritePin(LED_RED1_GPIO_Port, LED_RED1_Pin, SET);
-	HAL_GPIO_WritePin(LED_YELLOW1_GPIO_Port, LED_YELLOW1_Pin, SET);
-	HAL_GPIO_WritePin(LED_GREEN1_GPIO_Port, LED_GREEN1_Pin, RESET);
-	HAL_GPIO_WritePin(LED_RED2_GPIO_Port, LED_RED2_Pin, RESET);
-	HAL_GPIO_WritePin(LED_YELLOW2_GPIO_Port, LED_YELLOW2_Pin, SET);
-	HAL_GPIO_WritePin(LED_GREEN2_GPIO_Port, LED_GREEN2_Pin, SET);
-
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -180,32 +173,6 @@ int main(void)
 				display7SEG(count2/100);
 				HAL_GPIO_WritePin(LED7SEG_EN_2_GPIO_Port, LED7SEG_EN_2_Pin, RESET);
 			}
-  		/*if(state2 == 0) {
-			  if(count2 <= 0) {
-				  state2 = 1;
-				  count2 = 200;
-				  HAL_GPIO_TogglePin(LED_RED2_GPIO_Port, LED_RED2_Pin);
-				  HAL_GPIO_TogglePin(LED_YELLOW2_GPIO_Port, LED_YELLOW2_Pin);
-			}
-		  }
-		  if(state2 == 1) {
-			  if(count2 <= 0) {
-				  state2 = 2;
-				  count2 = 300;
-				  HAL_GPIO_TogglePin(LED_YELLOW2_GPIO_Port, LED_YELLOW2_Pin);
-				  HAL_GPIO_TogglePin(LED_GREEN2_GPIO_Port, LED_GREEN2_Pin);
-			  }
-		  }
-		  if(state2 == 2) {
-			  if(count2 <= 0){
-				  state2 = 0;
-				  count2 = 500;
-				  HAL_GPIO_TogglePin(LED_GREEN2_GPIO_Port, LED_GREEN2_Pin);
-				  HAL_GPIO_TogglePin(LED_RED2_GPIO_Port, LED_RED2_Pin);
-			  }
-		  }
-		  */
-
 
   		 count2--;
   	  	 count1--;
